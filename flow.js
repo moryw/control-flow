900a9fd39d49db1f63156e55795103980a81a0a3
moryw <matt@skitskat.xyz>
2018-04-30 12:15:10 +0700

    Add js file and sample code from lesson.

---
+++ flow.js
@@ -0,0 +1,14 @@
+//contorl flow lesson CC BWA
+
+let userName = '';
+let knowsJavaScript = false;
+
+if (knowsJavaScript && userName) {
+  console.log('Great, ' + userName + '! Get ready to practice your JavaScript!');
+} else if (knowsJavaScript) {
+  console.log('Great! Get ready to practice your JavaScript!');
+} else if (userName) {
+  console.log('Great, ' + userName + '! Get ready to learn something new!');
+} else {
+  console.log('Great! Get ready to learn something new!');
+}
