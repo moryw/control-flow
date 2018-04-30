411510d7782660ece60c0e088e6e89ef0cb53e88
moryw <matt@skitskat.xyz>
2018-04-30 14:34:41 +0700

    Refactor if/else statments with ternary opperators

--- flow.js
+++ flow.js
@@ -126,7 +126,38 @@
 // }
 
 
-
+// //ternary opperator for shorthand if/else statments
+// let isLocked = false;
+//
+// if (isLocked) {
+//   console.log('You will need a key to open the door.');
+// } else {
+//   console.log('You will not need a key to open the door.');
+// }
+// //refactor above statment
+// isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+//
+// let isCorrect = true;
+//
+// if (isCorrect) {
+//   console.log('Correct!');
+// } else {
+//   console.log('Incorrect!');
+// }
+//
+// //refactor above statment
+// isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+//
+// let favoritePhrase = 'Love That!';
+//
+// if (favoritePhrase === 'Love That!') {
+//   console.log('I love that!');
+// } else {
+//   console.log("I don't love that!");
+// }
+//
+// //refactor above statment
+// favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!");
 
 
 
